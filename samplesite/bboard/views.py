81b0ea431a76f9ef03a78c2c69e656280db8b347
burjui <burjui.dota@mail.ru>
2024-03-30 21:35:44 +0300

    первая модель

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Bb
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the bboard index.")
-# Create your views here.
+    bbs = Bb.objects.order_by("-published")
+    return render(request, "bboard/index.html", {"bbs": bbs})
+
+    # s = 'Обьявления\r\n\r\n\r\n'
+    # for bb in Bb.objects.order_by("-published"):
+    #     s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    # Create your views here.
