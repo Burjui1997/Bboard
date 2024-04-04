f9c5ed6107987d77bb8b7836dcca84a58d16023a
burjui <burjui.dota@mail.ru>
2024-04-04 21:49:04 +0300

    Создаем Форму

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Bb
+from .models import Bb, Rubric
+from django.views.generic.edit import CreateView
+from .forms import BbForm
 
 
+class BbCreateView(CreateView):
+    template_name = "bboard/bb_create.html"
+    form_class = BbForm
+    success_url = "/bboard/"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["rubrics"] = Rubric.objects.all()
+        return context
 def index(request):
     bbs = Bb.objects.all()
-    return render(request, "bboard/index.html", {"bbs": bbs})
+    rubrics = Rubric.objects.all()
+    context = {"bbs": bbs, "rubrics": rubrics}
+    return render(request, "bboard/index.html", context)
 
-    # s = 'Обьявления\r\n\r\n\r\n'
-    # for bb in Bb.objects.order_by("-published"):
-    #     s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    # Create your views here.
+
+def rubric_bbs(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {"bbs": bbs, "rubrics": rubrics,
+               "current_rubric": current_rubric}
+    return render(request, "bboard/rubric_bbs.html", context)
+
+
